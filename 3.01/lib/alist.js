12dd953150c98ff0c21db271c1962b17077d0908
yylxhjgy <159032635+yylxhjgy@users.noreply.github.com>
2024-04-18 11:52:13 +0800

    Add files via upload

---
+++ 3.01/lib/alist.js
@@ -0,0 +1,13 @@
+{
+"key": "alist",
+"name": "Alist",
+"type": 40,
+"api": "./alist.min.js",
+"ext": 
+[
+{"name":"九尾盘","server":"http://121.36.204.105:5678/"},
+{"name":"备用盘","server":"http://124.71.206.236:5678/","version": 3},
+{"name":"火影盘","server":"http://123.60.100.41:5678/"},
+{"name":"备用","server":"http://124.70.202.159:5678"},
+]
+}
